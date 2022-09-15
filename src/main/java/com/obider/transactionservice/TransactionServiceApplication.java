3a4735f27c43cb4680bed1b3fb053194652bfde6
Tobias <tobias.aditya@gmail.com>
2022-09-15 13:12:53 +0700

    implement middleware auth filter

--- src/main/java/com/obider/transactionservice/TransactionServiceApplication.java
+++ src/main/java/com/obider/transactionservice/TransactionServiceApplication.java
@@ -1,13 +1,36 @@
 package com.obider.transactionservice;
 
+import com.obider.transactionservice.middleware.AuthMiddleware;
+import com.obider.transactionservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TransactionServiceApplication {
+	private final UserService userService;
 
+	@Autowired
+	public TransactionServiceApplication(UserService userService) {
+		this.userService = userService;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionServiceApplication.class, args);
 	}
+
+	@Bean
+	public FilterRegistrationBean<AuthMiddleware> filterRegistrationBean(){
+		FilterRegistrationBean<AuthMiddleware> registrationBean = new FilterRegistrationBean<>();
+		AuthMiddleware authMiddleware = new AuthMiddleware(userService);
+		registrationBean.setFilter(authMiddleware);
+		registrationBean.addUrlPatterns("/api/v1/transaction/*");
+		return registrationBean;
+
+	}
+
+
+
 
 }
--- src/main/java/com/obider/transactionservice/exception/RestExceptionConstants.java
+++ src/main/java/com/obider/transactionservice/exception/RestExceptionConstants.java
@@ -1,11 +1,16 @@
 package com.obider.transactionservice.exception;
 
 public interface RestExceptionConstants {
+    //User exception
     String USR202_01 = "USR202_01";
     String USR202_02 = "USR202_02";
-
     String USR202_03 = "USR202_03";
+    //Auth exception
+    String USR203_01 = "USR203_01";
+    String USR203_02 = "USR203_02";
+    String USR203_03 = "USR203_03";
 
+    //Transaction exception
     String USR401_01 = "USR402_01";
     String USR401_02 = "USR402_02";
 
